7cd0e251a8b9739c67bbb803b39162bd55889e48
artyr77 <46862437+artyr77@users.noreply.github.com>
2019-03-03 19:15:17 +0300

    Update build97ebaa39081f256b.js

--- js/build97ebaa39081f256b.js
+++ js/build97ebaa39081f256b.js
@@ -1,40 +1,4 @@
-var config = {
-	 appName: 'HustBee', 
-	 apiUrl: window.location+'api/v1/method/',
-	 cdnUrl:'https://upbeat-cori-f6d03c.netlify.com/',
-	 token:'<? echo encode($_SESSION["id"]); ?>',
-	 panel:'Панель управления',
-	 profile:'Настройки',
-	 forum:'Форум',
-	 forumurl:'<? echo $forumurl; ?>',
-	 sitebuilder:'Конструктор сайтов',
-	 sitebuilderurl:'<? echo $builderurl; ?>',
-	 ifastnet:'iFastNet',
-	 ifastneturl:'https://ifastnet.com/portal/aff.php?aff=25986',
-	 create:{
-	   button2:"Создать бесплатный хостинг-аккаунт",
-	   myaccounts:"Мои аккаунты",
-       title:"Субдомен",
-       tooltip:"Выберите бесплатный поддомен. Вы можете добавить дополнительные домены позже.",
-       usefreedomain:"Использовать бесплатный субдомен",
-       usefreedomain1:"Вы можете создать бесплатный субдомен в зоне hustbee.gq",
-       userourdomain:"Использовать Ваш домен",
-       ourdomaintooltip:"Домен должен указывать на серверы имен: ns1.hustbee.tk and ns2.hustbee.tk.",
-       nameaccount:"Название аккаунта",
-       accpass:"Пароль",
-       createbutton:"Создать аккаунт",
-       login:"<? echo $login; ?>",
-       password:"<? echo $pass; ?>"
-	 },
-	 user:{
-      name:"<? echo $_SESSION["login"]; ?>",
-      email:"<? echo $_SESSION["email"]; ?>",
-      profileImage:"https://www.gravatar.com/avatar/<? echo md5($_SESSION["email"]); ?>?s=80"
-	 },
-	 storage: {
-	 	logourl:'https://firebasestorage.googleapis.com/v0/b/hustbee-ef687.appspot.com/o/storage%2Fimage-1.png?alt=media&token=d1ca2f0d-f826-4e87-87b0-5e7089af88c4'
-	 }
-	};  
+  
  var templateScript = $('#app').html();  
  var template = Handlebars.compile(templateScript);  
 $(document.body).append(template(config));
