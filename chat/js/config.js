39b3883b2644f6f5f13399ff98e117fafb1c8168
artyr77 <46862437+artyr77@users.noreply.github.com>
2019-03-06 23:50:07 +0300

    Create config.js

---
+++ chat/js/config.js
@@ -0,0 +1,9 @@
+ var config = {
+    apiKey: "AIzaSyCmI63YEuIUD3vTXUtFGVyq48oFsoTCR0Y",
+    authDomain: "hustvideos.firebaseapp.com",
+    databaseURL: "https://hustvideos.firebaseio.com",
+    projectId: "hustvideos",
+    storageBucket: "hustvideos.appspot.com",
+    messagingSenderId: "490880366914"
+  };
+  firebase.initializeApp(config);
